5ff541604232b2fed2ca7c8956df895d8ad57ff7
psudendra <psudendra@gmail.com>
2014-12-19 19:37:13 -0700

    Updated webpage

--- index.php
+++ index.php
@@ -1,91 +1,100 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Database Final</title>
+<style type="text/css">	
+body {
+	text-align:center;
+	color: black;
+	background-color: white;
+	font-family: Courier;
+}
 
-<!DOCTYPE html>
-	<head>
-		<title>Database Final</title>
-	</head>
-	<body>
-		<!-- This forms has the command text box and the submit button -->
-		<form method="post" action="<?php $_PHP_SELF ?>">
-			<table width="400" border="0" cellspacing="1" cellpadding="2">
-				<tr>
-					<!-- text that will go next to the text box -->
-					<td width="110">SQL Command:</td>
+fieldset {
+	font-size:1.0em;
+	background-color: lightgrey;
+}
 
-					<!-- text box /-->
-					<td><input name="sql_command" type="text" id="sql_command"></td>
-				</tr>
-				<tr>
-					<td width="100"> </td>
-					<td>
-						<!-- submit button -->
-						<input name="update" type="submit" id="update" value="Submit">
-					</td>
-				</tr>
-				<tr>
-					<td width="100"> </td>
-					<td> </td>
-				</tr>
-			</table>
-		</form>
-		<?php
-			if(isset($_POST['update']))
-			{
-				$dbhost = 'localhost';	// IP address / location of the db
-				$dbuser = 'root';			// username
-				$dbpass = '';			// password
-				$dbname = 'store';			// name of the db you want to access
-				$con = mysql_connect($dbhost, $dbuser, $dbpass);
+.fancy {
+	color: blue;
+	background-color: white;
+}
 
-				// check to make sure you are connected
-				if(! $con )
-				{
-					die('Could not connect: ' . mysql_error());
-				}
+</style>
+</head>
 
-				// store the sql command in a variable.
-				$sql = $_POST['sql_command'];
+<body>
+   
+<form method="post" action="<?php $_PHP_SELF ?>">
+<fieldset>
+<legend>Search the store database</legend>
+<p> 
+<label for="sql_command">Query: </label>
+<input name="sql_command" type="text" id="sql_command">
+</p> 
+<p>
+<input name="update" type="submit" id="update" value="Submit">
+</p>
+</fieldset>
+</form>
 
-				// select the db
-				mysql_select_db($dbname, $con);
+<?php
+	if(isset($_POST['update'])) {
+		$dbhost = 'localhost';	// IP address / location of the db
+		$dbuser = 'root';			// username
+		$dbpass = '';			// password
+		$dbname = 'store';			// name of the db you want to access
+		$con = mysql_connect($dbhost, $dbuser, $dbpass);
 
-				// run command and store results in $retval
-				$retval = mysql_query($sql, $con);
+		// check to make sure you are connected
+		if(! $con ) {
+			die('Could not connect: ' . mysql_error());
+		}
 
-				// if the mysql_query returned an error code, kill it and display the error
-				if(! $retval )
-				{
-					die('<br>Could not update data: ' . mysql_error());				
-				}
-				else
-				{	
-					// promp for successful update
-					echo "Updated data successfully<br>";
+		// store the sql command in a variable.
+		$sql = $_POST['sql_command'];
 
-					// display the return values in a table
-					echo "<table style='width:100%'>";	// begining table
-					$i=0;
-					echo '<tr>';
-					while($i < mysql_num_fields($retval)){
-						$meta=mysql_fetch_field($retval,$i);
-						echo '<td>'.$meta->name.'</td>';
-						$i=$i+1;
-					}
-					echo '</tr>';
-					
-					
-					while($record = mysql_fetch_row($retval)){
-						echo "<tr>";	// start table row
-						foreach($record as $key => $value){
-							echo "<td> ".$record[$key]." </td>";	// add one data element
-						}
-						echo "</tr>";	// end table row
-					}
-					echo "</table>";	// end table
-				}
+		// select the db
+		mysql_select_db($dbname, $con);
 
-				// close the connection
-				mysql_close($con);
+		// run command and store results in $retval
+		$retval = mysql_query($sql, $con);
+
+		// if the mysql_query returned an error code, kill it and display the error
+		if(! $retval ) {
+			die('<br>Could not update data: ' . mysql_error());				
+		}
+		else {	
+			// promp for successful update
+			print "Updated data successfully<br>";
+
+			// display the return values in a table
+			print "<table style='width:100%' class='fancy'>";	// begining table
+			$i=0;
+			print '<tr>';
+			while($i < mysql_num_fields($retval)){
+				$meta=mysql_fetch_field($retval,$i);
+				print '<td>'.$meta->name.'</td>';
+				$i=$i+1;
 			}
-		?>
-	</body>
+			print '</tr>';
+			
+			
+			while($record = mysql_fetch_row($retval)) {
+				print "<tr>";	// start table row
+				foreach($record as $key => $value){
+					print "<td> ".$record[$key]." </td>";	// add one data element
+				}
+				print "</tr>";	// end table row
+			}
+			print "</table>";	// end table
+		}
+
+		// close the connection
+		mysql_close($con);
+	}
+?>
+
+</body>
 </html>
